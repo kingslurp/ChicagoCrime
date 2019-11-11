dcd8716619ccd99152145748e14d95d54fd7c795
jonathan ray <jonathankay89@gmail.com>
2019-11-11 16:43:09 -0500

    # 3 - Beginnings of a GUI

--- ChicagoCrime.py
+++ ChicagoCrime.py
@@ -1,6 +1,24 @@
 import sqlite3
 from sqlite3 import Error
 import os
+import tkinter as tk
+
+
+class PythonGUI(tk.Frame):  # GUI Class
+    def __init__(self, master):
+        tk.Frame.__init__(self, master)
+        self.pack()
+        self.master.title("Chicago Crime Map")
+        tk.Label(self, text="Click 'Start' to Begin").pack()
+        tk.Button(self, text='Start', default='active', command=self.click_start).pack(side='left')
+        tk.Button(self, text='Cancel', command=self.click_cancel).pack(side='right')
+
+    def click_start(self):
+        print("The user clicked 'Start'")
+
+    def click_cancel(self):
+        print("The user clicked 'Cancel'")
+        self.master.destroy()
 
 
 class sqlconnect:
@@ -29,11 +47,15 @@ class sqlconnect:
 
 
 def main():
-    connecto = sqlconnect()
-    conn = connecto.create_connection()
-    with conn:
-        crime_id = connecto.queryIUCR(conn, '0486')
-        print(str(crime_id))
+    #connobject = sqlconnect()
+    #conn = connobject.create_connection()
+    #with conn:
+    #    crime_id = connobject.queryIUCR(conn, '0486')
+    #    print(str(crime_id))
+
+    root = tk.Tk()
+    app = PythonGUI(root)
+    app.mainloop()
 
 if __name__ == '__main__':
     main()
