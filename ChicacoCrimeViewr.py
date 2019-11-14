e86a9c5ff92cc16643618d45784b2eb3da847474
jonathan ray <jonathankay89@gmail.com>
2019-11-14 17:43:40 -0500

    # 8 - Started creation of class to handle the creation of the leafletjs api connected html document.

--- ChicacoCrimeViewr.py
+++ ChicacoCrimeViewr.py
@@ -3,6 +3,40 @@ from sqlite3 import Error
 import os
 import PySimpleGUI as sg
 from geopy import *
+from geopy.distance import great_circle
+
+
+class createReport:
+    def __init__(self):
+        report = open("reportfile.html", "w")
+        report.write("""
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>Quick Start - Leaflet</title>
+            <meta charset="utf-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="shortcut icon" type="image/x-icon" href="docs/images/favicon.ico" />
+            <link rel="stylesheet" href="https://unpkg.com/leaflet@1.5.1/dist/leaflet.css" integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ==" crossorigin=""/>
+            <script src="https://unpkg.com/leaflet@1.5.1/dist/leaflet.js" integrity="sha512-GffPMF3RvMeYyc1LWMHtK8EbPv0iNZ8/oTtHPx9/cc2ILxQ+u905qIwdpULaqDkyBKgOaB57QTMg7ztg8Jm2Og==" crossorigin=""></script>
+        </head>
+        <body>
+        
+        <div id="mapid" style="width: 600px; height: 400px;"></div>
+        <script>
+            var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+            L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+                maxZoom: 18,
+                attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+                    '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+                    'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+                id: 'mapbox.streets'
+            }).addTo(mymap);
+        </script>
+        </body>
+        </html>
+        """)
+        report.close()
 
 
 class sqlconnect:
@@ -51,6 +85,11 @@ class geolocater():
         print("Coordinates: " + coord)
 
 
+    def findDistance(self, start, finish):
+        distance_mi = great_circle(start, finish).miles
+        print(great_circle(start, finish).miles)
+        return distance_mi
+
 
 
 def main():
@@ -65,6 +104,9 @@ def main():
     newgeo = geolocater()
     newgeo.getAddress(33.9035792, -83.3390253)
     newgeo.getCoordinatePair("128, Milford Dr, Georgia, 30605")
+    SIU = (37.7100209, -89.2247828)
+    home_address = (33.9035792, -83.3390253)
+    newgeo.findDistance(SIU, home_address)
 
     # Demo of how columns work
     # GUI has on row 1 a vertical slider followed by a COLUMN with 7 rows
